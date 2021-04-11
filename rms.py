bbf4adaa2e7ba02c5e5e50bef76f4117bb137296
gc4764 <72872329+gc4764@users.noreply.github.com>
2021-04-11 23:26:16 +0530

    Create rms.py

---
+++ rms.py
@@ -0,0 +1,17 @@
+import kiteconnect
+import mysql
+import ./setting
+
+MAX_CAP = setting.MAX_CAP
+MIN_CAP = setting.MIN_CAP
+
+RISK_PERCENT = setting.RISK_PERCENT
+MAX_OPEN_TRADE = setting.MAX_OPEN_TRADE
+
+MAX_LOSS_PERCENT = setting.MAX_LOSS_PERCENT
+MAX_LOSS = setting.MAX_LOSS
+
+IS_INTRADAY = setting.IS_INTRADAY
+IS_DELIVERY = setting.IS_DELIVERY
+
+BROKER_NAME = setting.BROKER_NAME
